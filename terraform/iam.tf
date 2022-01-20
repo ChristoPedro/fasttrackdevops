061d9e5c236309b6593003b6d420f2ca733ed6da
ChristoPedro <pedroegcj@hotmail.com>
2022-01-20 11:02:10 -0300

    Name Update

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -8,14 +8,14 @@ resource "oci_identity_compartment" "devops_compartment" {
 resource "oci_identity_dynamic_group" "devops_dynamic_group" {
     #Required
     compartment_id = var.tenancy_ocid
-    description = "Devops DG"
+    description = "Devapp DG"
     matching_rule = "All {instance.compartment.id = '${local.compartment_ocid}'}"
     name = local.dg_name
 }
 
 resource "oci_identity_policy" "devops_policy" {
     compartment_id = oci_identity_compartment.devops_compartment.id
-    description = "Policy para as ferramentas de DevOps"
+    description = "Policy para as ferramentas de DevApp"
     name = "DevOps_Policy"
     statements = local.oci_policies_compartment
 }
@@ -29,9 +29,9 @@ resource "oci_identity_policy" "scanning_image_policy" {
 
 locals {
 
-    compartment_name = "DevOps"
+    compartment_name = "DevApp"
     compartment_ocid = oci_identity_compartment.devops_compartment.id
-    dg_name = "DevOpsDG"
+    dg_name = "DevApp"
 
     oci_policies_compartment = [
         "Allow any-user to use functions-family in compartment ${local.compartment_name} where ALL {request.principal.type= 'ApiGateway', request.resource.compartment.id = '${oci_identity_compartment.devops_compartment.id}'}",
